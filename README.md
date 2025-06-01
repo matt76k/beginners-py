84dae125ef3980a68ba1de4368139b690ec3c9f9
matt <matt76k@gmail.com>
2025-06-01 14:00:50 +0900

    表紙変更

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # beginners-py
 
 Describe your project here.
+
+magick cover.png -resize 1600x2560 cover_kindle.jpg
--- book/conf.py
+++ book/conf.py
@@ -3,24 +3,24 @@
 # If you wish to continue using _config.yml, make edits to that file and
 # re-generate this one.
 ###############################################################################
-project = 'Python魔法を極めて無双した件'
+project = 'Pythonを極めて無双した件'
 author = '鳳凰院 龍聖'
 comments_config = {'hypothesis': False, 'utterances': False}
 copyright = '2025, 鳳凰院 龍聖'
 epub_basename = 'BeginnersPy'
-epub_cover = ('_static/logo.png', '')
+epub_cover = ('_static/cover.png', '')
 exclude_patterns = ['**.ipynb_checkpoints', '.DS_Store', 'Thumbs.db', '_build']
 extensions = ['sphinx_togglebutton', 'sphinx_copybutton', 'myst_nb', 'jupyter_book', 'sphinx_thebe', 'sphinx_comments', 'sphinx_external_toc', 'sphinx.ext.intersphinx', 'sphinx_design', 'sphinx_book_theme', 'sphinx_proof', 'sphinx.ext.imgmath', 'sphinx_jupyterbook_latex', 'sphinx_multitoc_numbering']
 external_toc_exclude_missing = True
 external_toc_path = '_toc.yml'
 html_baseurl = ''
-html_favicon = 'logo.png'
+html_favicon = 'cover.png'
 html_js_files = ['https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.4/require.min.js']
-html_logo = 'logo.png'
+html_logo = 'cover.png'
 html_sourcelink_suffix = ''
 html_theme = 'sphinx_book_theme'
 html_theme_options = {'search_bar_text': 'Search this book...', 'launch_buttons': {'notebook_interface': 'jupyterlab', 'binderhub_url': '', 'jupyterhub_url': '', 'thebe': False, 'colab_url': '', 'deepnote_url': ''}, 'path_to_docs': '_sources', 'repository_url': 'https://github.com/matt76k/beginners-py', 'repository_branch': 'gh-pages', 'extra_footer': '', 'home_page_in_toc': True, 'announcement': '', 'analytics': {'google_analytics_id': '', 'plausible_analytics_domain': '', 'plausible_analytics_url': 'https://plausible.io/js/script.js'}, 'use_repository_button': False, 'use_edit_page_button': False, 'use_issues_button': False}
-html_title = '異世界転生！Python魔法を極めて無双した件 〜チートスキルゼロ！データの深淵を覗き、数値の混沌を操り、隠された真実を解き明かす存在へ〜'
+html_title = 'Pythonを極めて無双した件'
 imgmath_image_format = 'svg'
 language = 'ja'
 latex_engine = 'pdflatex'
